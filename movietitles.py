8091b35e7b31fed718e6ebb380038bec1db3a4b3
Jeff Trojan <j.dauda@alustudent.com>
2023-01-20 13:17:27 +0200

    Restructured movie match

--- movietitles.py
+++ movietitles.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 import re
-Movie Titles = re.compile(r'(.*)\s((\d{4}))')
+def movie_titles_match(titles):
+    titles = ""
+    movie_pattern = '(.*)\s((\d{4}))'
+    
+    is_movie_title = re.match(titles, movie_pattern)
+    if is_movie_title:
+        print(f"{titles} is a movie")
+    else:
+        print(f"{titles} is not a movie, please enter a proper movie name")
+
+
+# test 
+if __name__ == "__main__":
+    titles = ""
+    movie_titles_match(titles)
